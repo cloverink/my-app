01200072b9e741200f028cfe954482692044c84a
Aran(Tae) Chananar <aran.c@pomelofashion.com>
2022-07-04 15:54:02 +0700

    feat: back

--- src/components/Images/ResponsiveImage/ResponsiveImage.tsx
+++ src/components/Images/ResponsiveImage/ResponsiveImage.tsx
@@ -1,4 +1,10 @@
-import React, { MouseEventHandler, useCallback, useState } from 'react';
+import React, {
+  MouseEventHandler,
+  useCallback,
+  useEffect,
+  useRef,
+  useState
+} from 'react';
 import PlaceholderImage from '../PlaceholderImage';
 
 import classNames from 'classnames';
@@ -20,23 +26,24 @@ const ResponsiveImage: React.FC<ResponsiveImageType> = ({
   onClick,
   src
 }) => {
+  const ref = useRef<HTMLPictureElement>(null);
   const [isError, setIsError] = useState(false);
   const [isLoaded, setIsLoaded] = useState(false);
-  // const [placeHolderRatio, setPlaceHolderRatio] = useState(1);
-  // const [containerMaxHeight, setContainerMaxHeight] = useState(0);
+  const [placeHolderRatio, setPlaceHolderRatio] = useState(1);
+  const [containerMaxHeight, setContainerMaxHeight] = useState(0);
 
-  // useEffect(() => {
-  //   const handleResize = () => {
-  //     const width = ref.current?.clientWidth;
-  //     const height = width / imgRatio;
-  //     setContainerMaxHeight(height);
-  //     setPlaceHolderRatio(height / width);
-  //   };
+  useEffect(() => {
+    const handleResize = () => {
+      const width = ref.current?.clientWidth;
+      const height = width / imgRatio;
+      setContainerMaxHeight(height);
+      setPlaceHolderRatio(height / width);
+    };
 
-  //   handleResize();
+    handleResize();
 
-  //   window.addEventListener('resize', handleResize);
-  // }, [ref, imgRatio]);
+    window.addEventListener('resize', handleResize);
+  }, [ref, imgRatio]);
 
   const onLoad = useCallback(() => {
     setIsLoaded(true);
@@ -46,9 +53,14 @@ const ResponsiveImage: React.FC<ResponsiveImageType> = ({
   }, []);
 
   return (
-    <picture className={classNames('picture', className)} onClick={onClick}>
+    <picture
+      ref={ref}
+      className={classNames('picture', className)}
+      style={isLoaded ? {} : { maxHeight: containerMaxHeight }}
+      onClick={onClick}
+    >
       {(!isLoaded || isError) && (
-        <PlaceholderImage isStatic={isError} imgRatio={1} />
+        <PlaceholderImage isStatic={isError} imgRatio={placeHolderRatio} />
       )}
       {!isError && (
         <img
